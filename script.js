1c625a6981b0310ab25d06fc8f5ddc90b39e4d79
Davron01~ <d.mirabdullayev@mail.ru>
2022-12-04 21:27:47 +0500

    Completed the script

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-  $(document).ready(function () {
-    $('.counter').counterUp({
-      delay: 10,
-      time: 1200
-    });
+$(document).ready(function () {
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1200
   });
+});
 
 // scroll top element scripts here
 const toTop = document.querySelector('.to-top');
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 3000) {
+  if (window.pageYOffset > 100) {
     toTop.classList.add('active');
   } else {
-    toTop.classList.remove('active')
+    toTop.classList.remove('active');
   }
 
 })
